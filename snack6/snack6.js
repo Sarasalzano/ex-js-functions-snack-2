55fa8e030dca60dc87c9f199360f4d346152965a
Sara Salzano <sarasalzano95@gmail.com>
2025-12-12 10:08:04 +0100

    esegui snack 6

---
+++ snack6/snack6.js
@@ -0,0 +1,19 @@
+// Crea un contatore automatico con setInterval
+// Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo e restituisce una funzione che avvia un setInterval, incrementando un contatore e stampandolo.
+let contatore = 0;
+
+function incrementaContatore(intervallo) {
+  let interval;
+  interval = setInterval(() => {
+    contatore++;
+    console.log(contatore);
+  }, intervallo);
+}
+
+function creaContatoreAutomatico(intervallo) {
+    return function funzioneAvvio(){
+        return incrementaContatore(intervallo)
+    }
+}
+  
+console.log(creaContatoreAutomatico(2000)());
